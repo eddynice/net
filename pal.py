0eeb9e31395d28ac495d749e7303fcc6212e0f94
eddynics <osareniyeosazee@gmail.com>
2025-08-15 09:48:54 +0100

    first commit

---
+++ pal.py
@@ -0,0 +1,12 @@
+print("hello world")
+if 2 > 1:
+    print("2 is greater than 1")
+
+
+'''# pal.py
+This is a simple Python script that prints "hello world" and checks if 2 is greater'''
+
+
+# than 1, printing a message if true.
+
+print("2 is greater thanaa 1")
---
+++ script.js
@@ -0,0 +1,4 @@
+alert("welcome to second page")
+ name = "Alice";
+ age = 30;
+city = "New York";
